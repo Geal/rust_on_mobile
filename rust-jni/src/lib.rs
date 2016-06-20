eee23ed8a78d87a38a7cd6b06ef9b4f7bec25554
Geoffroy Couprie <geo.couprie@gmail.com>
2016-06-20 15:30:21 +0200

    use jni-sys instead of rewriting the structures

--- rust-jni/src/lib.rs
+++ rust-jni/src/lib.rs
@@ -1,19 +1,9 @@
 extern crate libc;
+extern crate jni_sys;
 extern crate inrustwetrust;
 
 use libc::{c_void, c_int};
-
-#[repr(C)]
-pub struct JNINativeInterface {
-    reserved0: *mut c_void,
-    reserved1: *mut c_void,
-    reserved2: *mut c_void,
-    reserved3: *mut c_void,
-    // much more actually in here for practical JNI code, but not
-    // relevant for this very simple example...
-}
-
-pub type JNIEnv = *const JNINativeInterface;
+use jni_sys::JNIEnv;
 
 #[allow(unused_variables,non_snake_case)]
 #[no_mangle]
